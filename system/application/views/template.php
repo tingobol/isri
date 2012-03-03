38f6447f7d8f3909ba7aa6adc69efd6152f7e890
ricardocasares@gmail.com <ricardocasares@gmail.com@ddfe61a8-5822-7ddd-ad74-618464d2b56c>
2012-03-03 03:06:38 +0000

    Nuevos cambios en la plantilla

--- system/application/views/template.php
+++ system/application/views/template.php
@@ -1,9 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>SGI</title>
+	<meta charset="utf-8">
 	<?=link_tag('static/css/yui.css')?>
 	<?=link_tag('static/css/sgi.css')?>
 	<?=link_tag('static/css/chat.css')?>
