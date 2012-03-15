44d0822cad2e8147a003dfd6558f9b179379c39f
ricardocasares@gmail.com <ricardocasares@gmail.com@ddfe61a8-5822-7ddd-ad74-618464d2b56c>
2012-03-15 06:38:19 +0000

    Cambiaron los nombres de las prioridades a Urgente, Importante, Normal, Diferido. (Fixes issue #13)

---
+++ trunk/Cambios.sql
@@ -0,0 +1,5 @@
+UPDATE `types` SET `type` = 'Urgente' WHERE `id` = '1' COLLATE utf8_bin LIMIT 1;
+UPDATE `types` SET `type` = 'Importante' WHERE `id` = '2' COLLATE utf8_bin LIMIT 1;
+UPDATE `types` SET `type` = 'Normal' WHERE `id` = '3' COLLATE utf8_bin LIMIT 1;
+UPDATE `types` SET `type` = 'Diferible' WHERE `id` = '4' COLLATE utf8_bin LIMIT 1;
+UPDATE `types` SET `type` = 'Informativo' WHERE `id` = '5' COLLATE utf8_bin LIMIT 1;
